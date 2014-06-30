193c64198178e7b8d4987d28dd091fec7405513c
Kelvin <lhq945@gmail.com>
2014-06-30 12:24:02 +0800

    test

--- public/js/behaviour/general.js
+++ public/js/behaviour/general.js
@@ -630,7 +630,7 @@ var App = function () {
   /*Data Tables*/
   var dataTables = function(){
   	//Basic Instance
-    var newtable = $("#datatable").dataTable({aLengthMenu:[[-1, 5, 50, 100, 200],["所有", 5, 50, 100, 200]]});
+    var newtable = $("#datatable").dataTable({aLengthMenu:[[10000000, 5, 50, 100, 200],["所有", 5, 50, 100, 200]]});
     newtable.fnSort([[0,'desc']]);
     //Search input style
     $('.dataTables_filter input').addClass('form-control').attr('placeholder','Search');
